3399973193fd9c02cbe36737e22c95a5a00220c0
NIMESH939 <nimeshkavindaofficial@gmail.com>
2024-09-20 21:16:03 +0530

    Blog List filter by categary

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -2,25 +2,25 @@ import { assets, blog_data } from "@/Assets/assets";
 import Image from "next/image";
 import React from "react";
 
-const BlogItem = () => {
+const BlogItem = ({ title, description, category, image }) => {
   return (
     <div className="max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]">
       <Image
-        src={blog_data[0].image}
+        src={image}
         alt=""
         width={400}
         height={400}
         className="border-b border-black"
       />
       <p className="ml-5 mt-5 px-1 inline-block bg-black text-white text-sm">
-        {blog_data[0].category}
+        {category}
       </p>
       <div className="p-5">
         <h5 className="mb-2 text-lg font-medium tracking-tight text-gray-900">
-          {blog_data[0].title}
+          {title}
         </h5>
         <p className="mb-3 text-sm tracking-tight text-gray-700">
-          {blog_data[0].description}
+          {description}
         </p>
         <div className="inline-flex items-center py-2 font-semibold text-center">
           Read More{" "}
