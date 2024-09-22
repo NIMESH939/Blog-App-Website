c9f6b3c8394db4cb1f43f0fb4ad36e76c07720a4
NIMESH939 <nimeshkavindaofficial@gmail.com>
2024-09-22 16:28:05 +0530

    Create API

---
+++ app/api/blog/route.js
@@ -0,0 +1,40 @@
+const { NextResponse } = require("next/server");
+import { ConnectDB } from "@/lib/config/db.js";
+import BlogModel from "@/lib/models/BlogModel";
+import { writeFile } from "fs/promises";
+
+const LoadDB = async () => {
+  await ConnectDB();
+};
+
+LoadDB();
+
+export async function GET(request) {
+  return NextResponse.json({ msg: "API Working" });
+}
+
+export async function POST(request) {
+  const formData = await request.formData();
+  const timestamp = Date.now();
+
+  const image = formData.get("image");
+  const imageByData = await image.arrayBuffer();
+  const buffer = Buffer.from(imageByData);
+  const path = `./public/${timestamp}_${image.name}`;
+  await writeFile(path, buffer);
+  const imgUrl = `/${timestamp}_${image.name}`;
+
+  const blogData = {
+    title: `${formData.get("title")}`,
+    description: `${formData.get("description")}`,
+    category: `${formData.get("category")}`,
+    author: `${formData.get("author")}`,
+    image: `${imgUrl}`,
+    authorImg: `${formData.get("authorImg")}`,
+  };
+
+  await BlogModel.create(blogData);
+  console.log("Blog Saved");
+
+  return NextResponse.json({ success: true, msg: "Blog Added" });
+}
