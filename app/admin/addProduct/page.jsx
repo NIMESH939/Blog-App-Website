76deb27dee4c468b2d81b359ae4fd44d03da958b
NIMESH939 <nimeshkavindaofficial@gmail.com>
2024-09-24 23:42:00 +0530

    Add Prodauct page

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -1,10 +1,105 @@
-'use client'
-import React from 'react'
+"use client";
+import { assets } from "@/Assets/assets";
+import axios from "axios";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const [image, setImage] = useState(false);
+  const [data, setData] = useState({
+    title: "",
+    description: "",
+    category: "Startup",
+    author: "Alex Bennett",
+    authorImg: "/author_img.png",
+  });
 
-export default page
+  const onChnageHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+    console.log(data);
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("title", data.title);
+    formData.append("description", data.description);
+    formData.append("category", data.category);
+    formData.append("author", data.author);
+    formData.append("authorImg", data.authorImg);
+    formData.append("image", image);
+
+    const response = await axios.post('/api/blog',formData);
+  };
+
+  return (
+    <>
+      <form
+        onSubmit={onSubmitHandler}
+        action=""
+        className="pt-5 px-5 sm:pt-12 sm:pl-16"
+      >
+        <p className="text-xl">Upload thumbnail</p>
+        <label htmlFor="image">
+          <Image
+            className="mt-4"
+            src={!image ? assets.upload_area : URL.createObjectURL(image)}
+            width={140}
+            height={70}
+            alt=""
+          />
+        </label>
+        <input
+          onChange={(e) => setImage(e.target.files[0])}
+          type="file"
+          id="image"
+          hidden
+          required
+        />
+        <p className="text-xl mt-4">Blog title</p>
+        <input
+          name="title"
+          onChange={onChnageHandler}
+          value={data.title}
+          type="text"
+          placeholder="Type here"
+          className="w-full sm:w-[500px] mt-4 px-4 py-3 border"
+          required
+        />
+        <p className="text-xl mt-4">Blog Description</p>
+        <textarea
+          name="description"
+          onChange={onChnageHandler}
+          value={data.description}
+          type="text"
+          placeholder="Write content here"
+          className="w-full sm:w-[500px] mt-4 px-4 py-3 border"
+          rows={6}
+          required
+        />
+        <p className="text-xl mt-4">Blog Category</p>
+        <select
+          name="category"
+          onChange={onChnageHandler}
+          value={data.category}
+          className="w-40 mt-4 px-4 py-4 border text-gray-500"
+        >
+          <option value="Startup">Startup</option>
+          <option value="Technology">Technology</option>
+          <option value="Lifestyle">Lifestyle</option>
+        </select>
+        <br />
+        <button
+          type="submit"
+          className="mt-8 mb-4 w-40 h-12 bg-black text-white"
+        >
+          ADD
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default page;
